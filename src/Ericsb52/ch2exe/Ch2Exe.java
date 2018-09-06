76d8feed538a0c6a6817b9c1cc855d0135259e86
ebroadbent <ebroadbent@CLC-RM209-TWS.TCSD.tooelesd.org>
2018-09-06 14:01:53 -0600

    ch2 exe code

---
+++ src/Ericsb52/ch2exe/Ch2Exe.java
@@ -0,0 +1,17 @@
+package Ericsb52.ch2exe;
+/*
+ Eric Broadbent
+ 9/18
+ ch2 exe
+ */
+
+public class Ch2Exe {
+	
+
+	public static void main(String[] args) {
+		// display some text
+		System.out.println(1/0);
+
+	}
+
+}
